c59ac9b3c42fd0a217d69bc3a82bc023a1876388
Samvel <samvel.antanyan@gmail.com>
2025-09-30 20:06:33 +0400

    Add authentication middleware and error handling for unauthorized access
    - Introduced NotAuthorizedError class for handling unauthorized access attempts.
    - Created currentUser middleware to attach the current user to the request object based on JWT.
    - Implemented requireAuth middleware to enforce authentication on protected routes.
    - Updated current-user route to utilize currentUser middleware for returning the authenticated user's information.
    - Modified signout route to clear the session upon signout.

--- src/routes/current-user.ts
+++ src/routes/current-user.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import { currentUser } from '../middlewares/current-user';
+import { Request, Response } from 'express';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  res.send('Current user');
-});
+router.get(
+  '/api/users/currentuser',
+  currentUser,
+  (req: Request, res: Response) => {
+    res.send({ currentUser: req.currentUser ?? null });
+  }
+);
 
 export { router as currentUserRouter };
